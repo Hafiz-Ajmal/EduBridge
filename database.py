28201fed5b3330716dd567c0e0d756c78ae7c3ce
Hafiz-Ajmal <hafizajmal144@gmail.com>
2026-07-18 15:59:26 +0500

    TEST_DB_URL just used in test folder else need to provide in render as secret

--- database.py
+++ database.py
@@ -12,8 +12,7 @@ print(os.getenv("DATABASE_URL"))
 DB_URL=os.getenv("DATABASE_URL")
 engine=create_engine(DB_URL)
 
-TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
-test_engine = create_engine(TEST_DATABASE_URL)
+
 
 #print( SQLModel.metadata.tables)
 
